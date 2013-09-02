014602779fcee52637d966f28a68d22ac94b9856
mikebirdgeneau <mike.birdgeneau@gmail.com>
2013-09-02 08:43:56 -0600

    Create dune-geometry.rb

---
+++ dune-geometry.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class DuneGeometry < Formula
+  homepage 'http://www.dune-project.org'
+  url 'http://www.dune-project.org/download/2.2.1/dune-geometry-2.2.1.tar.gz'
+  sha1 'f1f8b77a4c062d8662b8f4c44a378073568e1c87'
+
+  depends_on :fortran
+
+  def install
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
