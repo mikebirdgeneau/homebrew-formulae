1349c63e10059a3c1afab2181059ae0d00c20a4d
mikebirdgeneau <mike.birdgeneau@gmail.com>
2013-09-02 08:03:35 -0600

    Create opm-core.rb
    Added Formulae for opm-core. No tests included at this point.

---
+++ opm-core.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class OpmCore < Formula
+  homepage 'http://www.opm-project.org/'
+  url 'https://github.com/OPM/opm-core/archive/release/2013.03/final.tar.gz'
+  sha1 '538d8a55ef9a20d2af024f2547721f07ea3232b6'
+  version '2013.03'
+  depends_on 'cmake' => :build
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" 
+  end
+
+  test do
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test opm-core`.
+    system "true"
+  end
+end
